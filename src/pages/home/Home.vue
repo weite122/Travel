7df015f3de53423abe2eadf53fa1a5f6f02ccdf9
weite122 <949193531@qq.com>
2018-10-03 00:42:38 +0800

    [A]添加icon和优化配置的引入文件路径

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,38 +1,15 @@
 <template>
-  <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+  <div>
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
+  import HomeHeader from './components/Header'
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      HomeHeader
+    }
   }
 </script>
-
-<style lang="stylus" scoped>
-  .header
-    display: flex
-    height: .86rem
-    line-height: .86rem
-    background: #00bcd4
-    color: #fff
-    .header-left
-      width: .64rem
-      float: left
-    .header-input
-      flex: 1
-      background: #fff
-      margin-top: .1rem
-      height: .64rem
-      line-height: .64rem
-      margin-left: .2rem
-      border-radius: .1rem
-      color:#ccc
-    .header-right
-      width: 1.24rem
-      float: right
-      text-align: center
-</style>
