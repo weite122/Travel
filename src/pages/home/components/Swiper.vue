2d0cc18c6c1852dc2734a2f096b420eab189e2d2
weite122 <949193531@qq.com>
2018-10-03 01:38:57 +0800

    [A]添加header轮播组件

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeSwiper',
+    data() {
+      return {
+          swiperOption: {
+            pagination: '.swiper-pagination',
+            loop: true
+          },
+          swiperList: [{
+            id: '0001',
+            imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1809/87/534cf2e0a71fff02.jpg_750x200_5c778719.jpg'
+          },{
+            id: '0002',
+            imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
+          },{
+            id: '0003',
+            imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
+          }]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+     background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.67%
+   .swiper-img
+      width: 100%
+</style>
