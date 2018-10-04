2113d57d7c2902c8f3f988ebc545cceaf857a9f2
weite122 <949193531@qq.com>
2018-10-04 23:57:16 +0800

    [A]添加icon组件

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+      </div>
+      <p class="icon-description">热门景点</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeIcons"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .icons
+    overflow: hidden
+    height: 0
+    padding-bottom: 50%
+    background: yellow
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      background: green
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        background: blue
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-description
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        color: #333
+        text-align: center
+</style>
