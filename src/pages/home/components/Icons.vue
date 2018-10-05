bbfa61e2c63c3e259a7499332b8611a1e91e1049
weite122 <949193531@qq.com>
2018-10-05 23:58:50 +0800

    [A]添加icon组件

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -2,7 +2,7 @@
   <div class="icons">
     <div class="icon">
       <div class="icon-img">
-        <img class="icon-img-content" src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
       </div>
       <p class="icon-description">热门景点</p>
     </div>
@@ -20,7 +20,6 @@
     overflow: hidden
     height: 0
     padding-bottom: 50%
-    background: yellow
     .icon
       position: relative
       overflow: hidden
@@ -28,16 +27,12 @@
       width: 25%
       height: 0
       padding-bottom: 25%
-      background: green
       .icon-img
         position: absolute
         top: 0
         left: 0
         right: 0
         bottom: .44rem
-        box-sizing: border-box
-        padding: .1rem
-        background: blue
         .icon-img-content
           display: block
           margin: 0 auto
@@ -47,8 +42,8 @@
         left: 0
         right: 0
         bottom: 0
+        text-align: center
         height: .44rem
         line-height: .44rem
         color: #333
-        text-align: center
 </style>
