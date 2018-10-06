923467b7be6b8b6ec0e34d9bc5a7d55490f548a9
weite122 <949193531@qq.com>
2018-10-06 23:58:30 +0800

    [A]添加滑动效果

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
-      </div>
-      <p class="icon-description">热门景点</p>
-    </div>
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <div class="icon">
+          <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+          </div>
+          <p class="icon-description">热门景点</p>
+        </div>
+      </swiper-slide>
+
+      <swiper-slide>
+        <div class="icon">
+          <div class="icon-img">
+            <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+          </div>
+          <p class="icon-description">热门景点</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
@@ -16,34 +29,33 @@
 </script>
 
 <style lang="stylus" scoped>
-  .icons
-    overflow: hidden
+  .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-    .icon
-      position: relative
-      overflow: hidden
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: .44rem
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-description
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        text-align: center
-        height: .44rem
-        line-height: .44rem
-        color: #333
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-description
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      text-align: center
+      height: .44rem
+      line-height: .44rem
+      color: #333
 </style>
