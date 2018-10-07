1343fbfd50748764bdb4084430df0dd0dbcf2142
weite122 <949193531@qq.com>
2018-10-08 00:31:53 +0800

    [A]添加图标内容

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="icons">
     <swiper>
-      <swiper-slide>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
         <div
           class="icon"
-          v-for="item of iconList"
+          v-for="item of page"
           :key="item.id"
         >
           <div class="icon-img">
@@ -28,37 +28,54 @@
           "desc": "景点门票"
         }, {
           "id": "0002",
-          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png",
-          "desc": "滑雪季"
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
+          "desc": "一日游"
         }, {
           "id": "0003",
-          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png",
-          "desc": "泡温泉"
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+          "desc": "杭州必游"
         }, {
           "id": "0004",
-          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png",
-          "desc": "动植园"
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
+          "desc": "动植物园"
         }, {
           "id": "0005",
-          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png",
-          "desc": "游乐园"
+          "imgUrl": "https://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png",
+          "desc": "打卡圣地"
         }, {
           "id": "0006",
-          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png",
-          "desc": "必游榜单"
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1803/17/99402a22ce4af302.png",
+          "desc": "西湖"
         }, {
           "id": "0007",
-          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/17/4bd370f3eb1acd02.png",
-          "desc": "演出"
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png",
+          "desc": "杭州宋城景区"
         }, {
           "id": "0008",
-          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/7f/b1ea3c8c7fb6db02.png",
-          "desc": "城市观光"
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1803/b8/c5dcdb58deec2402.png",
+          "desc": "西溪湿地"
         }, {
           "id": "0009",
-          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png",
-          "desc": "一日游"
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1803/75/eca3ce656c886502.png",
+          "desc": "千湖岛"
+        }, {
+          "id": "0010",
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png",
+          "desc": "全部玩乐"
         }]
+      }
+    },
+    computed: {
+      pages () {
+        const pages = []
+        this.iconList.forEach((item, index) => {
+          const page = Math.floor(index / 8)
+          if (!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(item)
+        })
+        return pages
       }
     }
   }
@@ -66,6 +83,7 @@
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
   .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
@@ -82,6 +100,8 @@
       left: 0
       right: 0
       bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
       .icon-img-content
         display: block
         margin: 0 auto
@@ -90,9 +110,10 @@
       position: absolute
       left: 0
       right: 0
-      bottom: 0
+      bottom: .3rem
       text-align: center
       height: .44rem
       line-height: .44rem
       color: $darkTextColor
+      ellipsis()
 </style>
